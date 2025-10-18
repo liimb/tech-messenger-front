e6bc7fac75b86a828e2c082e32449ea45b86ed37
Артемий <freedomskyleva@gmail.com>
2025-10-18 16:12:13 +0600

    fix: home_screen

--- lib/modules/home/presentation/home_screen.dart
+++ lib/modules/home/presentation/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:tech_messenger/core/common/presentation/widget/app_icon.dart';
 import 'package:tech_messenger/core/constant/app_locales.dart';
